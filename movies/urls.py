88fabbbe6c1bad084327fe0fca4713a49fd4116c
kwoneyng <nan308@naver.com>
2019-11-27 12:44:27 +0900

    make db

--- movies/urls.py
+++ movies/urls.py
@@ -5,11 +5,7 @@ app_name = 'movies'
 
 urlpatterns = [
     path('', views.index, name='index'),
-<<<<<<< HEAD
-        path('push/', views.push, name='push'),
-=======
     path('push/', views.push, name='push'),
->>>>>>> 93d513fea96965d37ab694535e16c95d49a14034
     path('<int:movie_pk>/', views.detail, name='detail'),
     path('<int:movie_pk>/review/', views.review_create, name='review_create'),
     path('<int:movie_pk>/review/<int:review_pk>/delete/', views.review_delete, name='review_delete'),
